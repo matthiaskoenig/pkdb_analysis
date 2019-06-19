8be8b1c0cfa1726121e8d46ee081a621117814e1
Jan <janekg89@hotmail.de>
2019-06-19 16:14:57 +0200

    working on manuscript images

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup, find_packages
-setup(name='pkdb_analysis', version='1.0', packages=find_packages())
+
+setup(name='pkdb_analysis',
+      version='1.0',
+      packages=find_packages(),
+      package_data={
+            '': ['requirements.txt'],
+          },
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts': ['watch_study = data_management.watch_study:main'], },
+      )
