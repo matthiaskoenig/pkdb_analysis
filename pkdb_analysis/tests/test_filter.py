1850d0c6e4b3084d192bf4230859cae20eb6ef44
Jan Grzegorzewski <janekg89@hotmail.de>
2019-10-23 18:41:53 +0200

    tests fixed

--- pkdb_analysis/tests/test_filter.py
+++ pkdb_analysis/tests/test_filter.py
@@ -1,9 +1,9 @@
 import pytest
-from pkdb_analysis.filter import Filter, FilterFactory
+from pkdb_analysis.pkfilter import PKFilter, PKFilterFactory
 
 
 def test_empty_filter():
-    filter = Filter(normed=None)
+    filter = PKFilter(normed=None)
 
     assert len(filter.groups) == 0
     assert len(filter.individuals) == 0
@@ -13,25 +13,25 @@ def test_empty_filter():
 
     d = filter.to_dict()
     assert isinstance(d, dict)
-    for filter_key in Filter.KEYS:
+    for filter_key in PKFilter.KEYS:
         assert filter_key in d
 
 
 def test_study_sid_filter():
-    filter = FilterFactory.by_study_sid("PKDB00181")
-    assert isinstance(filter, Filter)
+    filter = PKFilterFactory.by_study_sid("PKDB00181")
+    assert isinstance(filter, PKFilter)
     d = filter.to_dict()
-    for filter_key in Filter.KEYS:
+    for filter_key in PKFilter.KEYS:
         assert filter_key in d
         assert "study_sid" in d[filter_key]
         assert d[filter_key]["study_sid"] == "PKDB00181"
 
 
 def test_study_name_filter():
-    filter = FilterFactory.by_study_name("Allonen1981")
-    assert isinstance(filter, Filter)
+    filter = PKFilterFactory.by_study_name("Allonen1981")
+    assert isinstance(filter, PKFilter)
     d = filter.to_dict()
-    for filter_key in Filter.KEYS:
+    for filter_key in PKFilter.KEYS:
         assert filter_key in d
         assert "study_name" in d[filter_key]
         assert d[filter_key]["study_name"] == "Allonen1981"
