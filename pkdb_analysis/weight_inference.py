a0f56887e4ec87c5553996b51a561966be25b927
Jan Grzegorzewski <janekg89@hotmail.de>
2019-12-16 16:28:03 +0100

    closes #5

--- pkdb_analysis/weight_inference.py
+++ pkdb_analysis/weight_inference.py
@@ -1,6 +1,7 @@
-
-import pint
 import numpy as np
+import pandas as pd
+import pint
+
 ureg = pint.UnitRegistry()
 
 
@@ -14,8 +15,7 @@ def infer_output(d):
     elif not np.isnan(d["mean_weight"]):
         weight = d["mean_weight"]
     else:
-        return None
-
+        return pd.Series(None, index=d.index)
     assert weight is not None, d
 
     if d["per_bodyweight"]:
@@ -32,8 +32,7 @@ def infer_output(d):
             d["per_bodyweight"] = not d["per_bodyweight"]
             return d
     else:
-        # if d["study_name"] == "Murphy1988":
-        #    print(d)
+
         if weight:
             result = u_unit * ((d["mean_weight"] * u_unit_weight) ** exponent)
             d["mean"] = result.m * d["mean"]
@@ -45,6 +44,8 @@ def infer_output(d):
             d["inferred"] = True
             d["per_bodyweight"] = not d["per_bodyweight"]
             return d
+        else:
+            return pd.Series(None, index=d.index)
 
 
 def infer_intervention(d):
@@ -58,7 +59,7 @@ def infer_intervention(d):
         weight = d["mean_weight"]
 
     else:
-        return None
+        return pd.Series(None, index=d.index)
 
     u_unit_weight = ureg(d["unit_weight"])
     assert weight is not None, d
@@ -85,14 +86,19 @@ def infer_intervention(d):
             d["per_bodyweight_intervention"] = not d["per_bodyweight_intervention"]
             return d
 
+
 def infer_weight(d):
     result_bodyweight = d.dropna(subset=["unit_weight"])
     result_no_bodyweight = d[d["unit_weight"].isnull()]
-    result_infer_outputs = result_bodyweight.apply(infer_output, axis=1).dropna(how="all")
+
+    result_infer_outputs = result_bodyweight.apply(infer_output, axis="columns").dropna(how="all")
+
     result_infer = result_bodyweight.append(result_infer_outputs, ignore_index=True)
-    result_infer_outputs = result_infer.apply(infer_intervention, axis=1).dropna(how="all")
+
+    result_infer_outputs = result_infer.apply(infer_intervention, axis="columns").dropna(how="all")
+
+
+
     result_infer = result_infer.append(result_infer_outputs, ignore_index=True)
     result_infer = result_infer.append(result_no_bodyweight, ignore_index=True)
     return result_infer
-
-
