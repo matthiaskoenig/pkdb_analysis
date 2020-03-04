f45f6a3625fe3b75081ae71cf54cae44a06ae34b
Jan Grzegorzewski <janekg89@hotmail.de>
2020-03-04 16:18:18 +0100

    refactor

--- pkdb_analysis/weight_inference.py
+++ pkdb_analysis/weight_inference.py
@@ -41,9 +41,6 @@ def infer_output(d):
                 result = u_unit * ((d["mean_weight"] * u_unit_weight) ** exponent)
                 for key in ["mean", "median", "min", "max", "sd", "se"]:
                     if isinstance(d[key], (float, np.ndarray)):
-                        if d["study_name"] == "Yu2004":
-                            print(result.m)
-
                         d[key] = result.m * d[key]
 
 
