b984b4ddac97068ed91898fd12206869395e9091
Matthias Koenig <konigmatt@googlemail.com>
2020-10-02 13:22:56 +0200

    fixes for release

--- src/pkdb_analysis/__init__.py
+++ src/pkdb_analysis/__init__.py
@@ -1,7 +1,6 @@
-__version__ = "0.1.6"
-
-
 from pkdb_analysis.data import PKData
 from pkdb_analysis.query import PKDB, PKFilter, PKFilterFactory
-
 from pkdb_analysis.query import query_pkdb_data
+
+
+__version__ = "0.1.5"
