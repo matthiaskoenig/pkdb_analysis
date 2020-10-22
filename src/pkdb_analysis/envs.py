9400ef7cf19ef694e83a0ee043bd52bcbb966094
Matthias Koenig <konigmatt@googlemail.com>
2020-10-22 18:12:39 +0200

    fixing environment variable issue

--- src/pkdb_analysis/envs.py
+++ src/pkdb_analysis/envs.py
@@ -7,6 +7,8 @@ from urllib import parse as urlparse
 
 from pkdb_analysis.logging_utils import bcolors
 
+# FIXME: this makes changing endpoints programatically extremely difficult.
+# FIXME: better environment variable names
 
 logger = logging.getLogger(__name__)
 
@@ -16,21 +18,27 @@ try:
     if BASE_URL.endswith("/"):
         BASE_URL = BASE_URL[:-1]
 
-    USER = os.environ["USER"]
-    PASSWORD = os.environ["PASSWORD"]
-
-    API_URL = BASE_URL + "/api/v1"
-
-except KeyError:
-
-    USER = None
-    PASSWORD = None
+except KeyError as err:
     BASE_URL = "https://alpha.pk-db.com"
-
-    API_URL = BASE_URL + "/api/v1"
+    logger.warning(f"No 'BASE_URL' set, using: '{BASE_URL}'")
     logger.warning(
         f"Environment variables have not been initialized. "
         f"1. add authentication credentials; and 2. run {bcolors.OKBLUE}set -a && "
         f"source .env.local{bcolors.ENDC}. "
         f"Queries will be performed as 'anonymous user' on endpoint '{BASE_URL}"
     )
+
+API_URL = BASE_URL + "/api/v1"
+
+try:
+    USER = os.environ["USER"]
+except KeyError as err:
+    USER = None
+    logger.warning(f"No 'USER' set, using: '{USER}'")
+
+try:
+    PASSWORD = os.environ["PASSWORD"]
+except KeyError as err:
+    PASSWORD = None
+    logger.warning(f"No 'PASSWORD' set, using: '{PASSWORD}'")
+
